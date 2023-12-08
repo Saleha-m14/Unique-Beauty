3bf9cd194ac6d5e4e4bb4b59662c9c4e0e92f244
Saleha-m14 <>
2023-12-08 21:05:26 +0000

    start the products app and add Category and Product modesl and regitster to admin page

---
+++ products/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Category model
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def __str__(self):
+        return self.friendly_name
+
+
+# Product model
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
