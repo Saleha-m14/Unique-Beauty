ee2f9c809669ea8aff496302c32ff2c8c462df6f
Saleha-m14 <>
2023-12-10 23:22:46 +0000

    add product data and create product_details page

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products')
+    path('', views.product_detail, name='product_detail')
+
 ]
--- products/views.py
+++ products/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 # Create your views here.
 
 def all_products(request):
+    """
+    This is a view to return all the products
+    """
     products = Product.objects.all()
     context = {
         'products': products
     }
     return render(request, 'products/products.html', context)
+
+def product_detail(request):
+    """
+    This view will return all the details of a product on a seprate page(product_detail.html)
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    context = {
+        'product': product
+    }
+    return render(request, 'products/product_detail.html', context)
 
 
