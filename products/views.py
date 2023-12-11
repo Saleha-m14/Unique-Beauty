3501c6f89d7f5c763904105e0289db42f7f16438
Saleha-m14 <>
2023-12-11 10:07:44 +0000

    add shopping_bag app and overall template

--- products/views.py
+++ products/views.py
@@ -12,13 +12,13 @@ def all_products(request):
     }
     return render(request, 'products/products.html', context)
 
-def product_detail(request):
+def product_detail(request, product_id):
     """
     This view will return all the details of a product on a seprate page(product_detail.html)
     """
     product = get_object_or_404(Product, pk=product_id)
     context = {
-        'product': product
+        'product': product,
     }
     return render(request, 'products/product_detail.html', context)
 
