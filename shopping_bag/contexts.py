547848b8ffbe9ce47cf38ab3311236bf1307cf4d
Saleha-m14 <>
2023-12-19 17:57:16 +0000

    worked on shopping bag template

--- shopping_bag/contexts.py
+++ shopping_bag/contexts.py
@@ -8,7 +8,7 @@ def shopping_bag_contents(request):
     shopping_bag_items = []
     product_count = 0
     total = 0
-    shopping_bag = request.session.get('bag', {})
+    shopping_bag = request.session.get('shopping_bag', {})
 
     for item_id, quantity in shopping_bag.items():
         product = get_object_or_404(Product, pk=item_id)
@@ -28,7 +28,7 @@ def shopping_bag_contents(request):
         free_delivery_delta = 0
     grand_total = delivery + total
 
-    contexts = {
+    context = {
         'shopping_bag_items': shopping_bag_items,
         'total': total,
         'product_count': product_count,
@@ -38,4 +38,4 @@ def shopping_bag_contents(request):
         'grand_total': grand_total,
     }
 
-    return contexts
+    return context
