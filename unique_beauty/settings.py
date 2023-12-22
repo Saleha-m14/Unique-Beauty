bfefd10a05fff9aff79505f761064c525f224523
Saleha-m14 <>
2023-12-22 23:19:33 +0000

    open the project on a new workspace

--- unique_beauty/settings.py
+++ unique_beauty/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-zd@!aj)y$_$^%8leo+u9-)c$%_eq1#72z58o!(b4bdsz+vey)q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-saleha-m14-unique-beauty-ps6qmq3mgv.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['8000-saleha-m14-unique-beauty-f2hhkrtqo4.us2.codeanyapp.com']
 CSRF_TRUSTED_ORIGINS = ['https://8000-saleha-m14-unique-beauty-ps6qmq3mgv.us2.codeanyapp.com']
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
