a5f9e5e69470119d0de95bb451ea3ff1238f9049
Saleha-m14 <>
2023-12-24 20:32:37 +0000

    created the base template for the checkout.html file

--- unique_beauty/settings.py
+++ unique_beauty/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'products',
     'shopping_bag',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +62,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'unique_beauty.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -75,6 +80,10 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'shopping_bag.contexts.shopping_bag_contents',
+            ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
             ],
         },
     },
