cc2b3eb0becc2e37e353f5e4f9999ebaac1b0e67
Saleha-m14 <>
2023-12-24 17:55:45 +0000

    fixed typing mistake

--- checkout/models.py
+++ checkout/models.py
@@ -56,7 +56,7 @@ class Order(models.Model):
 
 
 class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='Lineitems')
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
@@ -70,4 +70,4 @@ class OrderLineItem(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'SKY {self.product.sku} on order {self.order.order_number}' 
+        return f'SKU {self.product.sku} on order {self.order.order_number}' 
