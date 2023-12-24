9d551fece5b3c09d0a22c7d369d2a80c06b7462b
Saleha-m14 <>
2023-12-24 17:53:49 +0000

    added the OrderLineItem model

--- checkout/models.py
+++ checkout/models.py
@@ -3,6 +3,7 @@ import uuid
 from django.db import models
 from django.conf import settings
 from django.db.models import Sum
+from products.models import Product
 
 # Order
 
@@ -15,16 +16,16 @@ class Order(models.Model):
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
-    street_address2 = models.CharField(max_length=80, null=False, blank=False)
-    country = models.CharField(max_length=80, null=True, blank=True)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     
-    def __generate_order_number(self):
+    def _generate_order_number(self):
         """
-        Generate a rndom, unique order number using uuid
+        Generate a random, unique order number using uuid
         """
         return uuid.uuid4().hex.upper()
 
@@ -53,4 +54,20 @@ class Order(models.Model):
     def __str__(self):
         return self.order_number
 
-        
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='Lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        This def will override the original save method
+        to set the lineitem total and update the order total
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKY {self.product.sku} on order {self.order.order_number}' 
