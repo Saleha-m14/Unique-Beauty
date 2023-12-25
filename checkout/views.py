017f3d9fe6142f394864892f518385a05f8ae0b0
Saleha-m14 <>
2023-12-25 09:58:05 +0000

    added stripe elements to the checkout

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51ORAZ3D4bddW0mwgJumHxeLLNEL8cBCkQKt8P6oKwlbKM78TC9klca4hVXXsPbSDgWc0TVBFRKC3B458SjXow3Yt00ILmy4Dbl',
+        'client_secret': 'test client secret',
     }
     
     return render(request, template,)
