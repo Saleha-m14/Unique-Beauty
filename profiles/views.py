d4533d1d616de349f8799cdc2412edbdcc9d8747
Saleha-m14 <>
2023-12-25 22:52:59 +0000

    created the profile view, url and basic template

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Profile View
+def profile(request):
+    """ Display the user's profile. """
+
+    template = 'profiles/profile.html'
+    context = {}
+
+    return render(request, template, context)
