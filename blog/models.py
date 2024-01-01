6f4a075283229657d2d8c181f1d60b9a0028ebbe
Saleha-m14 <>
2024-01-01 09:13:51 +0000

    created the blog post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    content = models.TextField(blank=False)
+    featured_image = models.ImageField('blog_image', default='placeholder')
+    alt_tag = models.CharField(max_length=50, default='Blog image')
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+def __str__(self):
+    return self.title
