09ce9d9765c519991d3b93db86ebd72a1dea966c
Saleha-m14 <>
2024-01-14 23:37:21 +0000

    created the contact form, model, view and template

--- home/models.py
+++ home/models.py
@@ -1,5 +1,12 @@
 from django.db import models
-
+from django.core.validators import EmailValidator
 # Create your models here.
 # contact form
 
+class Contact(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField()
+    subject = models.CharField(max_length=100)
+    message = models.TextField()
+    def __str__(self):
+        return self.name
--- home/views.py
+++ home/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-
+from .forms import ContactForm
+from django.contrib import messages
 # Create your views here.
 
 def index(request):
-    return render(request, 'home/index.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # Process the form data
+            form.save(commit=False)
+            messages.success(request, 'Thanks for contacting us!')
+        else:
+            messages.error(request, 'Please fill the contact form to get in touch.')
+    form = ContactForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'home/index.html', context)
