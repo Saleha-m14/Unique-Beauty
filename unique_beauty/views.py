655d33531ef8c7099d1619c2e8419fb24d5655b5
Saleha-m14 <>
2024-01-22 21:20:00 +0000

    removed the space from views.py file

--- unique_beauty/views.py
+++ unique_beauty/views.py
@@ -6,6 +6,5 @@ def handler404(request, exception):
     return render(request, "errors/404.html", status=404)
 
 def handler500(request):
-   """ Error Handler 500 - Server Error """
-
+    """ Error Handler 500 - Server Error """
     return render(request, "errors/500.html", status=500)
